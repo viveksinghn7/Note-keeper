7fb8a0ffd1917c45fdf2b161d1b60d1a21c45f3a
viveksinghn7 <work.viveksinghnegi@gmail.com>
2024-09-23 02:32:12 +0530

    Update title and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 function Footer() {
-  const year = new Date().getFullYear();
+  // const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright © {year} | Note Keeper</p>
+      <p>Copyright © 2024 | Note Keeper</p>
       <p>Made with ❤️ by Viveksingh Negi using ReactJS</p>
     </footer>
   );
